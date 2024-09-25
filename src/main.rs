3e4a1e5f97f2fa5156558fea8a33eda67c537b01
dimitri <dkoutsis@gmail.com>
2024-09-25 10:42:17 +0300

    main driver code

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,34 @@ mod deck;
 mod analyser;
 mod predictor;
 
-fn main() {
+use std::io::Write;
 
-    let mut deck = 0;
+fn main() {
+    print!("Enter number of opponents: ");
+    std::io::stdout().flush().unwrap();
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    println!("{}", buf);
+    let opponents: u8 = buf.trim().parse().unwrap();
+    print!("Enter your cards: ");
+    std::io::stdout().flush().unwrap();
+    buf.clear();
+    std::io::stdin().read_line(&mut buf).unwrap();
+    let mut it = buf.split(' ');
+    let deck: deck::Deck = deck::to_deck(&[it.next().unwrap(), it.next().unwrap()]);
+    let mut players = vec![deck];
     
-    let mut i = 0;
-    while deck != 0xfffffffffffff {
-        deck += deck::get_random_card(deck);
-        println!("{}", i);
-        i += 1;
+    for _ in 0..opponents {
+        buf.clear();
+        print!("Enter opponent's cards: ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut buf).unwrap();
+        let mut it = buf.split(' ');
+        let deck: deck::Deck = deck::to_deck(&[it.next().unwrap(), it.next().unwrap()]);
+        players.push(deck);
     }
 
+    let (odds, tie_odds) = crate::predictor::predict(&players);
+    println!("Winning odds {:.2}. Tie odds {:.2}.", odds * 100f32, tie_odds * 100f32);
 }
 
